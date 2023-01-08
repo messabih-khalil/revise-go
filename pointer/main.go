a13695e2272c28faabaf9ffc1c1e9b892b505035
messabih-khalil <msbih.khalil@gmail.com>
2023-01-09 00:33:18 +0100

    pointer

--- pointer/main.go
+++ pointer/main.go
@@ -14,6 +14,25 @@ func (p * Product) statusChanger(){
 	p.security = !p.security
 }
 
+// func checkOut(ps * []Product)  {
+// 	for _ , v := range Product{
+// 		v.
+// 	}
+// }
+
+const (
+	inactive = false
+	active = true
+)
+
 func main(){
-	
+	items := make([]Product , 4)
+
+	items[0] = Product{
+		item: Item{
+			name : "xbox",
+			price: 2000.0,
+		},
+		security: active,
+	}
 }
