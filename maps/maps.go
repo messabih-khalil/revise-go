bd4b8218faba759367991085f67518285356f2ec
messabih-khalil <msbih.khalil@gmail.com>
2023-01-09 00:22:59 +0100

    maps

--- maps/maps.go
+++ maps/maps.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+type Name string
+
+type Product struct{
+	name string
+	price int
+	inStock bool
+}
+
 func main(){
-	
+	shopingList := make(map[Name]Product)
+
+	shopingList["product 1"] = Product{"xbox" , 5000 , true}
+	shopingList["product 2"] = Product{"ps5" , 5000 , false}
+	shopingList["product 3"] = Product{"iphone 14" , 5000 , true}
+
+	for k , v := range shopingList{
+		fmt.Println(k , "\nname : " , v.name ,"\nprice : " , v.price , "\navailable : " , v.inStock)
+	}
 }
