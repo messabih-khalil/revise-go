8cb22fcea73bb3e346a6eb40e15bf20467106736
messabih-khalil <msbih.khalil@gmail.com>
2023-01-08 23:17:33 +0100

    structs

--- structs.go
+++ structs.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+
+type Student struct{
+	name string
+	highSchool bool
+}
+
+type ClassRoom struct{
+	students [3]Student
+}
+
+
 func main(){
-	fmt.Printf("Hello wordl")
+	aldin := Student{"aldin" , true}
+
+	var (
+		tars = Student{"tars" , false}
+		james = Student{"james" , true}
+	)
+
+	class := ClassRoom{}
+
+	class.students[0] = aldin
+	class.students[1] = tars
+	class.students[2] = james
+
+	// show details 
+
+	for _ , v := range class.students {
+		if v.highSchool {
+			fmt.Println(v.name , "Is on high school")
+		}
+	}
 }
