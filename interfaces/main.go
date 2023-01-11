ed6e99dfec02704b4ca9e6ba1bbc56b62aad49ef
messabih-khalil <msbih.khalil@gmail.com>
2023-01-11 01:58:47 +0100

    create interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,7 +1,22 @@
 package main
 
+type Modifier interface{
+	health(h int)
+	shield(s int)
+}
 
+type Player struct{
+	playerHealth int
+}
+type Monster struct{
+	monsterHealth int
+}
+
+
+func (p * Player) health(h int){
+
+}
 
 func main(){
-	
+
 }
